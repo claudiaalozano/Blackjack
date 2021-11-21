7ab46d67724f3f5766329999396d8b7824866050
claudiaalozano <clau.lozano.perez@gmail.com>
2021-11-21 23:07:54 +0100

    commit 0

---
+++ blackjack.py
@@ -0,0 +1,8 @@
+def valor_carta(valor,cartamesa,conteo):
+    cartamesa += 1
+    for valor_carta[:1] in ( 1, 2, 3 , "Q", "K"):
+        conteo += 1
+        return
+    for valor_carta[:1] in (2, 3, 4, 5, 6, 7, 8, 9):
+        conteo += 1
+        return 
